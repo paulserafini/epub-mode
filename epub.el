e4b116da398919c2cab4ac717312f0c0ae892c8f
paulserafini <pserafin@sfu.ca>
2018-08-25 16:09:02 -0700

    Add todo regarding _ bug, add some comments

--- epub.el
+++ epub.el
@@ -3,6 +3,7 @@
 (defvar epub-mode-map
   (let ((map (make-keymap)))
     (define-key map (kbd "M-n") 'epub-next-section)
+    (define-key map (kbd "M-o") 'epub-open-file)
     (define-key map (kbd "M-p") 'epub-previous-section)
     map)
   "Keymap for epub")
@@ -11,10 +12,8 @@
   "Proceed to the next section"
 
   (interactive)
-
   (if (< current-chapter number-of-sections)
       (setq current-chapter (+ current-chapter 1)))
-
   (epub-open-section))
 
 (defun epub-open-file ()
@@ -24,12 +23,10 @@
 
   (setq epub-file (read-file-name "Choose an .epub file: "))
 
-  (setq epub-directory (concat "/tmp/epub-mode/" (number-to-string (random 10000))))
-
   ;; extract files
+  (setq epub-directory (concat "/tmp/epub-mode/" (number-to-string (random 10000))))
   (start-process "" nil "unzip" epub-file "-d" epub-directory)
-
-  (sleep-for 10)
+  (sleep-for 10) ;; temporary, to make sure the files are unzipped before it tries to open them
 
   ;; find the .opf file
   (setq find-opf (concat "find " epub-directory " -regex" " .*.opf"))
@@ -50,7 +47,7 @@
 
   ;; define some important variables
   (setq number-of-sections (length manifest))
-  (setq current-chapter 5)
+  (setq current-chapter 0)
   (setq relative-directory (file-name-directory (directory-file-name opf-file)))
 
   (epub-open-section))
@@ -72,10 +69,8 @@
   "Go back to a previous section"
 
   (interactive)
-
   (if (> current-chapter 0)
       (setq current-chapter (- current-chapter 1)))
-
   (epub-open-section))
 
 (defun epub-mode ()
